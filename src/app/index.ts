f4341332ed710441cf682add9f94223f6ec86af0
Diego Ramos <diegorbrito9@gmail.com>
2024-08-01 19:48:32 -0300

    chore: create api template

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 import fastify from "fastify";
 import helmet from "@fastify/helmet";
 import cors from "@fastify/cors";
+import apiDocs from "@docs/api-docs.json";
+
+import fastifyApiReference from "@scalar/fastify-api-reference";
 
 import { errorMiddleware } from "@/middleware/error-middleware";
 import { appRoutes } from "@/modules/app-routes";
@@ -10,6 +13,15 @@ const app = fastify();
 
 app.register(helmet, { global: true });
 app.register(cors);
+app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    title: "Example API doc",
+    spec: {
+      content: apiDocs,
+    },
+  },
+});
 
 app.setErrorHandler(errorMiddleware);
 app.register(appRoutes);
