a05c4433a87ac281fde263366d30310a430e1337
Diego Ramos <diegorbrito9@gmail.com>
2024-08-01 18:04:15 -0300

    feat: create error handling

---
+++ src/middleware/error-middleware.ts
@@ -0,0 +1,10 @@
+import { FastifyError, FastifyReply, FastifyRequest } from 'fastify';
+import { Exception } from '@/utils/exception';
+
+export function errorMiddleware(err: FastifyError, req: FastifyRequest, reply: FastifyReply) {
+  if (err instanceof Exception) {
+    return reply.status(err.statusCode).send({ error: err.message });
+  }
+
+  return reply.status(500).send({ error: err.message });
+}
